0bacdc54764dfdf38edd393c58b210c39f612634
ping.guan <ping.guan@cyber-insight.com>
2020-12-19 16:41:58 +0800

    promise

--- 01_promise/作业/chain.js
+++ 01_promise/作业/chain.js
@@ -1,10 +1,58 @@
+const p1 = new Promise(resolve => {
+  setTimeout(() => {
+    resolve()
+  }, 1000)
+})
+
+const p2 = new Promise(resolve => {
+  setTimeout(() => {
+    resolve()
+  }, 2000)
+})
+
+const p3 = new Promise(resolve => {
+  setTimeout(() => {
+    resolve()
+  }, 3000)
+})
+
+const p4 = new Promise(resolve => {
+  setTimeout(() => {
+    resolve()
+  }, 4000)
+})
+
 /**
  * 实现promise链式调用，当上一个promise完成之后，才执行下一个promise
+ * 1、async await
+ * 2、reduce
+ * 
  */
+const chain1 = async(list = []) => {
+  for(let p of list) {
+    await p()
+  }
+}
 
+const chain2 = (list = []) => {
+  list.reduce((c,p) => {
+    c.then(p)
+  }, Promise.resolve())
+}
+
+const list = [p1,p2,p3,p4]
+chain1(list)
+chain2(list)
 
  /**
   * 实现promise的批次链式调用
   * 设置一个阀值，同时执行N个promise, 其中任任意一个promise完成后，替补上另一个promise
   * 简单来说，也就是需求池上，始终有N个promise在同时执行
   */
+ const chainThreshold = (n = 2) => {
+  // 把阈值填满
+  let res = [list.shift(), list.shift()]
+
+  
+ }
+
--- 01_promise/源码/promise.js
+++ 01_promise/源码/promise.js
@@ -1,5 +1,98 @@
 class CustomPromise {
+  constructor(handler) {
+    this.status = 'Pending';
+    this.value = undefined
+    this.fulfilledFn = []
+    this.rejectedFn = []
 
+    try {
+      handler(triggerResolve, triggerReject)
+    } catch(err) {
+      console.log(err)
+    }
+  }
+
+  triggerResolve(val) {
+    setTimeout(() => {
+      if(this.status !== 'Pending') return 
+      this.status = 'Fulfilled'
+      this.value = val
+      this.fulfilledFn.forEach(v => v(this.value))
+      this.fulfilledFn = []
+    },0)
+  }
+
+  triggerReject(val) {
+    if(this.status === 'Pending') {
+      setTimeout(() => {
+        this.rejectedFn.forEach(v => v())
+      })
+      this.status = 'Rejected'
+    }
+  }
+
+  then(onFulfilled, onRejected) {
+    const then2 = new CustomPromise((onNextFulfilled, onNextRejected) => {
+      // 核心：链式调用的处理过程
+      // 将上一个promise的结果与下一个then的promise串联起来
+      function onFinalFulfilled(val) {
+        if(typeof onFulfilled !== 'function') {
+          onNextFulfilled(val)
+        } else {
+          const res = onFulfilled(val)
+          // 判断返回的是否是个promise
+          if(res && res.then && typeof res.then === 'function') {
+            res.then(onNextFulfilled)
+          } else {
+            onNextFulfilled(res)
+          }
+        }
+      }
+
+      switch(this.status) {
+        case 'Pending': {
+          this.fulfilledFn.push(onFinalFulfilled)
+          break
+        }
+      }
+    })
+
+    return then2
+  }
+
+  static catch(onRejected) {
+    return this.then(null, onRejected)
+  }
+
+  static all(list) {
+    return new CustomPromise((resolve, reject) => {
+      let count = 0
+      const values = []
+      for(const [i, customPromiseInstance] of list.entries()) {
+        customPromiseInstance
+          .then(res => {
+            values[i] = res
+            count ++
+
+            if(count === list.length) {
+              resolve(values)
+            }
+          }, err => {
+            reject(err)
+          })
+      }
+    })
+  }
+
+  static resolve(val) {
+    return new CustomPromise(resolve => resolve(val))
+  }
+
+  static reject(val) {
+    return new CustomPromise(function(resolve, reject) {
+      reject()
+    })
+  }
 }
 
 const createPromise = function(time) {
