5d5d5ca7ecfc8b8a661d3186db06d24644ce06f5
官萍 <ping.guan@cyber-insight.com>
2020-12-24 16:48:48 +0800

    promise总结

--- 01_promise/作业/chain.js
+++ 01_promise/作业/chain.js
@@ -1,31 +1,32 @@
 const p1 = new Promise(resolve => {
   setTimeout(() => {
-    resolve()
-  }, 1000)
+    resolve(10)
+  }, 10000)
 })
 
 const p2 = new Promise(resolve => {
   setTimeout(() => {
-    resolve()
+    resolve(20)
   }, 2000)
 })
 
 const p3 = new Promise(resolve => {
   setTimeout(() => {
-    resolve()
+    resolve(30)
   }, 3000)
 })
 
 const p4 = new Promise(resolve => {
   setTimeout(() => {
-    resolve()
+    resolve(40)
   }, 4000)
 })
 
 /**
  * 实现promise链式调用，当上一个promise完成之后，才执行下一个promise
- * 1、async await
- * 2、reduce
+ * 思路：等待上一个promise完成之后，再执行下一个
+ * 1、async await：等到上一个结果，才执行下一个
+ * 2、reduce：把下一个作为上一个primise的then函数的参数传递进去
  * 
  */
 const chain1 = async(list = []) => {
@@ -45,14 +46,10 @@ chain1(list)
 chain2(list)
 
  /**
-  * 实现promise的批次链式调用
+  * 实现promise的并发-阈值功能
   * 设置一个阀值，同时执行N个promise, 其中任任意一个promise完成后，替补上另一个promise
   * 简单来说，也就是需求池上，始终有N个promise在同时执行
   */
- const chainThreshold = (n = 2) => {
-  // 把阈值填满
-  let res = [list.shift(), list.shift()]
-
+const asyncPool = () => {
   
- }
-
+}
