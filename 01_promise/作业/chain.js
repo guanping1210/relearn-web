44ab79677cbb675942e8ab40ddcf8fbd2392c7f6
官萍 <ping.guan@cyber-insight.com>
2020-12-25 16:22:44 +0800

    test

--- 01_promise/作业/chain.js
+++ 01_promise/作业/chain.js
@@ -47,9 +47,29 @@ chain2(list)
 
  /**
   * 实现promise的并发-阈值功能
-  * 设置一个阀值，同时执行N个promise, 其中任任意一个promise完成后，替补上另一个promise
+  * 设置一个阀值，同时并发执行N个promise, 其中任任意一个promise完成后，替补上另一个promise
   * 简单来说，也就是需求池上，始终有N个promise在同时执行
+  * 
+  * 场景设想：排队买票，一共三个窗口可以同时进入三个人，任一窗口买完票之后，排队的队列再弹出一个人
+  *          始终保持三个窗口都在运行
+  * https://blog.csdn.net/weixin_40005545/article/details/111350426
   */
-const asyncPool = () => {
-  
+const asyncPool = (promiseList, limit) => {
+  let i = 0
+  // 存储所有的Promise实例的结果
+  const result = []
+  // 用于存储正在执行的Promise的实例
+  const executing = []
+
+  const enqueue = function() {
+    if(i >= promiseList.length) {
+      return Promise.resolve()
+    }
+    // 拿到当前实例
+    const item = promiseList[i++]
+    // 当前promise执行完毕，从executing数组中删除
+    const p = item.then(() => executing.splice(executing.indexOf(exec)))
+  }
+
+  return enqueue().then(() => Promise.all(promiseList))
 }
