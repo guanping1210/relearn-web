e0ed2df98e94b2de02cf9fbbc6645676337c6fd4
ping.guan <ping.guan@cyber-insight.com>
2021-01-19 22:25:19 +0800

    test

---
+++ 06_ES6/01_let-const.js
@@ -0,0 +1,18 @@
+/**
+ * 题目1: 用let或const改写这段使用var的旧程序
+ */
+var subTotal = 19.9
+var tax = 0.13
+var total = subTotal * (1 + tax)
+console.log(total)
+
+
+/**
+ * 题目2: 用let或者const改写
+ * 如果成绩在60分以上，则课程pass
+ */
+var point = 95
+if(point >= 60) {
+  var pass = true
+}
+console.log(pass)
---
+++ 06_ES6/03_map+set.js
@@ -0,0 +1,13 @@
+/**
+ * 题目1: 判断下面代码打印的值
+ */
+const set = new Set()
+set.add(1)
+set.add(1)
+set.add('1')
+
+console.log(set)
+
+/**
+ * 
+ */
