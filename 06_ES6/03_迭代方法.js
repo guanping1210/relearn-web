2a032d911f7a4949612eb1c4947e25a4d6079ba9
ping.guan <ping.guan@cyber-insight.com>
2021-01-28 22:30:58 +0800

    迭代

---
+++ 06_ES6/03_迭代方法.js
@@ -0,0 +1,14 @@
+/**
+ * 1、数组：forEach, map,filter, some, every
+ */
+var arr = [1,2,3]
+
+// 当前元素，元素下标，数组本身
+arr.forEach((value, index, array) => {
+
+})
+
+// 创建筛选后的新数组, 返回的是个新数组、
+arr.filter((value, index, array) => {
+
+})
