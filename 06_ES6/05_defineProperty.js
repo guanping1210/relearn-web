984e4bef67e6f757ddfba36156ff9fc4e5d3cf38
官萍 <ping.guan@cyber-insight.com>
2021-02-01 18:09:24 +0800

    test

--- 06_ES6/05_defineProperty.js
+++ 06_ES6/05_defineProperty.js
@@ -4,19 +4,25 @@
  */
 
 var star = {
-    sing: 100
+    // sing: 100
 }
 
 //  ES5添加属性
-star.sing = 100
+// star.sing = 100
 
 // es6添加属性
 Object.defineProperty(star, 'dance', {
     value: 1000,
-    
+    writable: false, // 是否允许目标属性的修改， 默认是true
+    enumerable: true, // 目标属性是否可以被枚举，默认是false, 默认不允许被遍历，也就是Object.keys这些是遍历不到的
+    configurable: true, // 目标属性是否可以被删除，默认是false
 })
+
+star.dance = 10 // 修改失败，dance属性的值还是1000
 
 // es6修改sing属性
 Object.defineProperty(star, 'sing', {
     value: 30000
 })
+
+console.log(star)
