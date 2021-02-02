3690e4da8567f1dca26aa0231e33a4d8bb26118b
ping.guan <ping.guan@cyber-insight.com>
2021-02-02 22:37:27 +0800

    test

--- 06_ES6/14_解构赋值.js
+++ 06_ES6/14_解构赋值.js
@@ -10,3 +10,19 @@ c // 3
 // 对象解构：按照属性名对应
 // 可以赋值默认值，以及别名
 let { name, age: ageName } = { name: 'heelo', age: 20 }
+
+
+/**
+ * 扩展运算符: ...
+ *  1、能够将伪数组变为真的数组，主要用于DOM集合
+ *  2、Array.from(), 也能将伪数组转变为真正的数组
+ */
+
+var arrayLike = {
+  "0": 'zhangsan',
+  "1": 'lisi',
+  "2": 'wangwu',
+  "length": 3
+}
+
+Array.from(arrayLike)
