208831338b68c2d6dd685149fa635f76e96e91eb
官萍 <ping.guan@cyber-insight.com>
2021-02-22 17:53:40 +0800

    study

---
+++ 001_ 面试/必备手写题/reduce.js
@@ -0,0 +1,28 @@
+// @ts-nocheck
+/**
+ * https://www.jianshu.com/p/e375ba1cfc47
+ * 
+ * reduce(callback, initial)
+ *  callback有四个参数：prev与cur的类型不一定一致
+ *      prev: 第一项或上一次叠加的结果值
+ *      cur: 当前参与叠加的项
+ *      index: 当前值的索引
+ *      arr: 数组本身
+ *  initial: 如果没有这个值，会默认取数据的第一项作为初始值
+ *
+ */
+var arr = [1,2,3,4]
+var sum = arr.reduce((prev, cur, index, arr) => {
+    return prev + cur
+}, 0)
+
+console.log(sum) // 1+2+3+4 = 10
+
+var result = [
+    { score: 80 },
+    { score: 90 },
+    { score: 88 },
+]
+var score = result.reduce((prev, cur, index, arr) => {
+    return cur.score + prev
+}, 0)
