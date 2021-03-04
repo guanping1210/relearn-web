b051e780ac1f649df64e73d0f40751e58d4c9ddf
官萍 <ping.guan@cyber-insight.com>
2021-03-04 18:05:02 +0800

    test

--- 000_leetcode/29_乘积最大子序列.js
+++ 000_leetcode/29_乘积最大子序列.js
@@ -13,6 +13,10 @@ function maxProduct(nums) {
     let max = nums[0]
 
     for(let i = 1; i < nums.length; i ++) {
-
+        maxProductMemo[i] = Math.max(nums[i], nums[i] * maxProductMemo[i - 1], nums[i] * minProductMemo[i - 1])
+        minProductMemo[i] = Math.max(nums[i], nums[i] * maxProductMemo[i - 1], nums[i] * minProductMemo[i - 1])
+        max = Math.max(max, maxProductMemo[i])
     }
+
+    return max
 }
