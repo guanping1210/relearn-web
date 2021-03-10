a4be26377b9bea7b349c2ff10fd65b9781400fb9
官萍 <ping.guan@cyber-insight.com>
2021-03-10 13:38:18 +0800

    test

--- test.js
+++ test.js
@@ -1,3 +1,37 @@
-const a = require('./package.json')
+var calculate = function(s) {
+    const ops = [1], n = s.length
+    let sign = 1, ret = 0, i = 0
 
-console.log(a.__esModule)
+    while(i < n) {
+        if(s[i] === ' ') { // 空格
+            i ++
+        } else if(s[i] === '+') {
+            sign = ops[ops.length - 1]
+            i ++
+        } else if(s[i] === '-') {
+            sign = -ops[ops.length - 1]
+            i ++
+        } else if(s[i] === '(') {
+            ops.push(sign)
+            i ++
+        } else if(s[i] === ')') {
+            ops.pop()
+            i ++
+        } else {
+            let num = 0
+
+            // 判断到s[i], 是非数字就走人
+            while(i < n && !(isNaN(Number(s[i]))) && s[i] !== ' ') {
+                debugger
+                num = num * 10 + s[i].charCodeAt() - '0'.charCodeAt()
+                i ++
+            }
+
+            ret += sign * num
+        }
+    }
+
+    return ret
+};
+
+calculate('1 +(9 - 1)')
