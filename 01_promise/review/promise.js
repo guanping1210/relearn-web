0e3f936952c6f4b4ff29e6111e5db8c4c5a9c597
ping.guan <ping.guan@cyber-insight.com>
2021-08-02 21:24:07 +0800

    review

--- 01_promise/review/promise.js
+++ 01_promise/review/promise.js
@@ -59,20 +59,37 @@ promise5.then(res => {
 
 
 // promise 和 x 指向同一对象，会TypeError
-const promise6 = new Promise(resolve => {
-    resolve(promise6)
-})
+// const promise6 = new Promise(resolve => {
+//     resolve(promise6)
+// })
 
 // resolve 静态方法
-const promise7 = Promise.resolve(123)
+const promise7 = Promise.resolve(123);
 promise7.then(res => {
-    console.log('已完成：', res)
-})
+    console.log('已完成：', res);
+});
 
 // reject 静态方法
-const promise8 = Promise.resolve(123)
+const promise8 = Promise.resolve(123);
 promise8.then(null, res => {
-    console.log('已拒绝：', res)
+    console.log('已拒绝：', res);
+});
+
+// all 静态方法: 接收一个数组，所有状态都已完成，则进入完成状态(注意接收的是数组)
+// 多个promise是同时进行的
+const promise9 = Promise.all([promise1(), promise2()]);
+promise9.then(res => {
+    // all 返回的是一个数组 [10, 20]
+    console.log('all: ', res);
+});
+
+// race静态方法：接收一个数组，只要一个promise状态改变，就进行该状态且不可改变
+// 所有的promise实例是竞争关系，只选择第一个修改状态的promise的值
+// 多个promise同时进行
+var promise10 = Promise.race([promise1(), promise2(), promise3()])
+promise10.then(res => {
+    // race 返回的是最先改变状态的那个值
+    console.log('race: ', res)
 })
 
 /**
@@ -86,5 +103,27 @@ promise8.then(null, res => {
  * 7、then方法返回的是一个新的promise实例
  * 8、不能用instanceof 判断是否是 promise的实例，因为这是一类规范，并不是统一的构造函数
  * 9、判断是不是promise，就判断是不是有then函数，根据特性来进行判断
- * 10、静态方法 resolve、reject、
+ * 10、then方法可以调用多次，并且then内部的方法按照顺序执行
+ * 10、静态方法 resolve、reject、all、race
+ * 
+ * 
+ * 相比较promise的串行调用，后续ES6中出现的generator/async await 更加简洁
  */
+
+ function* gen() {
+     yield promise1()
+     yield promise2()
+ }
+
+ var g = gen()
+ g.next()
+ g.next()
+
+
+ async function asycnAwait () {
+    const r1 = await promise1()
+    const r2 = await promise2()
+    console.log(r1, r2)
+ }
+
+ asycnAwait() // 打印10， 20
