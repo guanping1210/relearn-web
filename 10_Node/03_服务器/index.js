704bcd8acaef342f6fb410ea129c97ddc6b51dd8
官萍 <ping.guan@cyber-insight.com>
2021-10-08 16:40:57 +0800

    node

---
+++ 10_Node/03_服务器/index.js
@@ -0,0 +1,15 @@
+/**
+ * 端口：
+ * URL：统一资源定位符，也就是所说的网址
+ */
+
+const http = require('http')
+// 创建web服务器, app对象就是网站服务器对象
+const app = http.createServer()
+// 当客户端发送请求的时候
+app.on('request', (req, res) => {
+    // 响应
+    res.end('<h4>hello</h4>')
+})
+app.listen(9000)
+console.log('服务器已经启动，监听9000端口，通过localhost:9000访问')
